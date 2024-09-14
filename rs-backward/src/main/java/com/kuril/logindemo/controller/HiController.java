4719df13a12541a8bc75213e22f35c6e2675bab3
CodeDance2023 <lqw2860689098@qq.com>
2024-09-15 00:44:45 +0800

    mirror分支提交

--- rs-backward/src/main/java/com/kuril/logindemo/controller/HiController.java
+++ rs-backward/src/main/java/com/kuril/logindemo/controller/HiController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HiController {
     @GetMapping("/hi")
     public String hi() {
-        return "hi";
+        return "hello";
     }
 }
