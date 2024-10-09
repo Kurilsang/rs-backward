24d24b20e14181cc0f5576c395b9b2d015ca8158
CodeDance2023 <lqw2860689098@qq.com>
2024-10-09 23:02:54 +0800

    模块初始化

---
+++ rs-block/src/test/java/com/chenpihuangliang/rsblock/RsBlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenpihuangliang.rsblock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsBlockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
