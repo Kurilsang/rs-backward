ea57287820f1e5a7f2f19c29a96403e50a8267ff
CodeDance2023 <lqw2860689098@qq.com>
2024-10-09 22:55:52 +0800

    模块初始化

---
+++ rs-block/src/main/java/com/chenpihuangliang/rsblock/RsBlockApplication.java
@@ -0,0 +1,13 @@
+package com.chenpihuangliang.rsblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsBlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsBlockApplication.class, args);
+	}
+
+}
